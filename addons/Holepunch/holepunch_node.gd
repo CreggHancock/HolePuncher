4564edc6384c3c302652855efc534e08c2176607
Luke <thecookielegends@gmail.com>
2022-07-25 17:19:14 -0500

    switch to name indexing

--- addons/Holepunch/holepunch_node.gd
+++ addons/Holepunch/holepunch_node.gd
@@ -136,7 +136,7 @@ func _process(delta):
 					var clientdata = packet_string.split(",") #this is formatted client:ip:port,client2:ip:port
 					for c in clientdata:
 						var m = c.split(":")
-						peers[m[1]] = {"port":m[2], "address":("localhost" if local_testing else m[1]),"hosting":m[3]}
+						peers[m[1]] = {"port":m[2], "address":("localhost" if local_testing else m[1]),"hosting":m[3],"name":m[4]}
 					recieved_peer_info = true
 					start_peer_contact()
 				else:
@@ -144,17 +144,17 @@ func _process(delta):
 					handle_failure("No peers found.") #report to game to handle accordingly
 
 #handle peer greet; reconfigure ports
-func _handle_greet_message(peer_ip, peer_port):
-	if peer_stages[peer_ip] == 0: peer_stages[peer_ip] = 1
-	peers[peer_ip].port = peer_port
+func _handle_greet_message(peer_name, peer_port):
+	if peer_stages[peer_name] == 0: peer_stages[peer_ip] = 1
+	peers[peer_name].port = peer_port
 
-func _handle_confirm_message(peer_ip,peer_port):
-	peer_stages[peer_ip] = 2
-	peers[peer_ip].port = peer_port #why not
+func _handle_confirm_message(peer_name,peer_port):
+	peer_stages[peer_name] = 2
+	peers[peer_name].port = peer_port #why not
 
-func _handle_go_message(peer_ip,peer_port):
-	peer_stages[peer_ip] = 2
-	if peers[peer_ip].hosting:
+func _handle_go_message(peer_name,peer_port):
+	peer_stages[peer_name] = 2
+	if peers[peer_name].hosting:
 		emit_signal("hole_punched", int(own_port), int(host_port), host_address)
 		p_timer.stop()
 		set_process(false)
@@ -172,34 +172,36 @@ func _handle_go_message(peer_ip,peer_port):
 func _ping_peer():
 	var all_info = true
 	var all_confirm = true
-	for peer in peers:
+	for p in peers.keys():
+		var peer = peers[p]
 		if not peer.address in peer_stages:
-			peer_stages[peer.address] = 0
-		stage = peer_stages[peer.address]
+			peer_stages[peer.name] = 0
+		var stage = peer_stages[peer.address]
 		if stage < 1: all_info = false
 		if stage < 2: all_confirm = false
 		if stage == 0: #received no contact, send greet
 			print("send greet!")
 			peer_udp.set_dest_address(peer.address, int(peer.port))
 			var buffer = PoolByteArray()
-			buffer.append_array((PEER_GREET+str(own_address)+":"+str(own_port)).to_utf8())
+			buffer.append_array((PEER_GREET+client_name+":"+str(own_port)).to_utf8())
 			peer_udp.put_packet(buffer)
 		if stage == 1 and recieved_peer_greets:
 			print("send confirm!")
 			peer_udp.set_dest_address(peer.address, int(peer.port))
 			var buffer = PoolByteArray()
-			buffer.append_array((PEER_CONFIRM+str(own_address)+":"+str(own_port)).to_utf8())
+			buffer.append_array((PEER_CONFIRM+client_name+":"+str(own_port)).to_utf8())
 			peer_udp.put_packet(buffer)
 	if all_info:
 		recieved_peer_greets = true
 	if all_confirm:
 		recieved_peer_confirms = true
 		if is_host:
-			for peer in peers:
+			for p in peers.keys():
+				var peer = peers[p]
 				print("send go!")
 				peer_udp.set_dest_address(peer.address, int(peer.port))
 				var buffer = PoolByteArray()
-				buffer.append_array((HOST_GO+str(own_address)+":"+str(own_port)).to_utf8())
+				buffer.append_array((HOST_GO+client_name+":"+str(own_port)).to_utf8())
 				peer_udp.put_packet(buffer)
 			emit_signal("hole_punched", int(own_port), int(host_port), host_address)
 			p_timer.stop()
