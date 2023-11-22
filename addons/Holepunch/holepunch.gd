f00206465cbc34728fc53703fa5ded0ab3ea9720
Brandon Thacker <brandon.thacker@thackertech.io>
2023-11-22 10:22:37 -0500

    port to godot 4

--- addons/Holepunch/holepunch.gd
+++ addons/Holepunch/holepunch.gd
@@ -1,10 +1,10 @@
-tool
+@tool
 extends EditorPlugin
 
 func _enter_tree():
-	# Initialization of the plugin goes here
-	add_custom_type("HolePunch", "Node", preload("holepunch_node.gd"), preload("icon.png"))
+    # Initialization of the plugin goes here
+    add_custom_type("HolePunch", "Node", preload("holepunch_node.gd"), preload("icon.png"))
 
 func _exit_tree():
-	# Clean-up of the plugin goes here
-	remove_custom_type("HolePunch")
+    # Clean-up of the plugin goes here
+    remove_custom_type("HolePunch")
